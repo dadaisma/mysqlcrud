873e74588a328b9246cac30dcdf032881a18a657
dadaisma <infodadaisma@gmail.com>
2023-10-19 15:01:05 +0200

    c

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -15,7 +15,7 @@ const ProductCard = ({ product }) => {
 
 
   return (
-    <Link href={`/products/${product.id}`} key={product.id}>
+    
       <div className='bg-gray-100'>
       <div className="bg-gray-300  shadow border-2 rounded grid grid-cols-1 md:grid-cols-4 justify-center text-center ml-10 mr-10  items-center ">
           <p className='font-bold text-xl'>{product.name}</p>
@@ -31,13 +31,13 @@ const ProductCard = ({ product }) => {
           
           <div className="flex items-center justify-center mt-3 mb-3">
       <button className="bg-blue-500 text-white rounded-lg w-1/2">
-        View/Edit
+      <Link href={`/products/${product.id}`} key={product.id}> View/Edit</Link> 
       </button>
     </div>
          
         </div>
       </div>
-    </Link>
+    
   );
 };
 
